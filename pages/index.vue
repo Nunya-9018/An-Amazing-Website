7a19cb5819c0ac3e43dda6306e1ca4603cff271d
Tyler Campbell <73594766+Nunya-9018@users.noreply.github.com>
2022-07-15 22:24:14 +0000

    inital commit, new base files with basic functionality

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <HomeContent/>
+</template>
+
+<script>
+import HomeContent from '../components/Home-content.vue';
+export default {
+    name: "IndexPage",
+    components: { HomeContent }
+}
+</script>
