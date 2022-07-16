2ae9fa7a9730cf379b0e741079d4bec0d1b46fee
Tyler Campbell <73594766+Nunya-9018@users.noreply.github.com>
2022-07-16 12:56:17 +0000

    deleted files for testing

--- components/Home-content.vue
+++
@@ -1,8 +0,0 @@
-<template>
- <div class="relative flex items-top justify-center min-h-screen bg-gray-800 sm:items-center sm:pt-0">
-    <div class="mt-8 bg-gray-600 overflow-hidden w-11/12 h-1/2 shadow text-center sm:rounded-lg p-6">
-        <h1>welcome</h1>
-
-    </div>
-</div>
-</template>
