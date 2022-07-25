c9e52dcc6b595a682f5b59520a7f6697ac1a86b8
Tyler Campbell <73594766+Nunya-9018@users.noreply.github.com>
2022-07-25 12:41:47 +0000

    edited footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
 <div class="relative flex justify-center bg-gray-800 ">
     <div class="bg-gray-600 overflow-hidden w-screen h-11 shadow  text-center sm:rounded-tl-md sm:rounded-tr-md p-3">
         <div class="pt-0.1">
-            <h1 class="">by Tyler Campbell</h1>
+            <h1 class="">By Tyler Campbell</h1>
         </div>
     </div>
 </div>
