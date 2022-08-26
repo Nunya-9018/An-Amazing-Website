c68fabb6f6393b93c7924db414de22b410b7ec33
nunya-9018 <007youdontknowme@gmail.com>
2022-08-26 08:52:55 -0400

    added space

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sm:pt-2">
     <div class="bg-neutral-900 text-violet-700 text-center w-2/12 ml-auto mr-auto p-3 rounded-lg grid grid-cols-3 divide-x divide-black">
-        <div><NuxtLink to="/" class="no-underline hover:underline hover:text-violet-500">Home</NuxtLink></div>
+        <div><NuxtLink to="/" class="no-underline hover:underline hover:text-violet-500 ">Home</NuxtLink></div>
         <div class="blog"><NuxtLink to="/blog" class="no-underline hover:underline hover:text-violet-500">Blog</NuxtLink></div>
         <div><NuxtLink to="/about" class="no-underline hover:underline hover:text-violet-500">About</NuxtLink></div>
     </div>
